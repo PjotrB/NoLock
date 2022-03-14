f3cfc2d4a17ab59c1a38b057be16386575b27ea3
Peter Broekhuizen <peter.broekhuizen@pwc.com>
2022-03-14 16:03:57 +0100

    Add project files.

---
+++ NoLock/EXECUTION_STATE.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+[FlagsAttribute]
+public enum EXECUTION_STATE : uint
+{
+    ES_AWAYMODE_REQUIRED = 0x00000040,
+    ES_CONTINUOUS = 0x80000000,
+    ES_DISPLAY_REQUIRED = 0x00000002,
+    ES_SYSTEM_REQUIRED = 0x00000001
+    // Legacy flag, should not be used.
+    // ES_USER_PRESENT = 0x00000004
+}
---
+++ NoLock/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Runtime.InteropServices;
+
+Console.WriteLine("NoLock started: " + DateTime.Now.ToShortTimeString());
+
+while (true) {
+    await Task.Delay(TimeSpan.FromMinutes(5));
+    Console.WriteLine("NoLock SetThreadExecutionState: " + DateTime.Now.ToShortTimeString());
+    PreventSleep();
+}
+
+void PreventSleep()
+{
+    SetThreadExecutionState(EXECUTION_STATE.ES_CONTINUOUS | EXECUTION_STATE.ES_DISPLAY_REQUIRED);
+  //SetThreadExecutionState(EXECUTION_STATE.ES_CONTINUOUS | EXECUTION_STATE.ES_AWAYMODE_REQUIRED);
+}
+
+[DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+static extern EXECUTION_STATE SetThreadExecutionState(EXECUTION_STATE esFlags);
