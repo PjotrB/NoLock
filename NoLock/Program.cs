e778dee55a003bb55b30ec0b442a983f75718e7d
Peter Broekhuizen <pjotrb@gmail.com>
2022-03-14 16:58:18 +0100

    Cleanup, improve comment

--- NoLock/Program.cs
+++ NoLock/Program.cs
@@ -1,18 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// Created using .NET 6 Console Application template that uses "top-level statements".
+// See https://aka.ms/new-console-template for more information.
+
 using System.Runtime.InteropServices;
 
 Console.WriteLine("NoLock started: " + DateTime.Now.ToShortTimeString());
 
-while (true) {
+while (true)
+{
     await Task.Delay(TimeSpan.FromMinutes(5));
     Console.WriteLine("NoLock SetThreadExecutionState: " + DateTime.Now.ToShortTimeString());
     PreventSleep();
 }
 
-void PreventSleep()
+static void PreventSleep()
 {
     SetThreadExecutionState(EXECUTION_STATE.ES_CONTINUOUS | EXECUTION_STATE.ES_DISPLAY_REQUIRED);
-  //SetThreadExecutionState(EXECUTION_STATE.ES_CONTINUOUS | EXECUTION_STATE.ES_AWAYMODE_REQUIRED);
 }
 
 [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
